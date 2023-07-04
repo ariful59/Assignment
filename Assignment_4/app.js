2c585a6e70b5bd73547e1afa7f73500fc243869a
Md. Ariful Amin <mdarifulamin@Mds-MacBook-Air.local>
2023-07-04 21:45:14 +0600

    assignment_partial

---
+++ Assignment_4/app.js
@@ -0,0 +1,37 @@
+import { products } from "./product.js";
+
+function displayProductList(){
+    let productList =  document.getElementById("productList");
+    productList.innerHTML = ' ';
+    products.forEach(function(product){
+        let productRaw = document.createElement('div');
+        productRaw.classList.add('w-full', 'max-w-sm', 'ml-10', 'mt-2' ,'mx-4', 'bg-gray-100', 'border', 'border-gray-400', 'rounded-lg', 'shadow', 'dark:bg-gray-800', 'dark:border-gray-700');
+        productRaw.innerHTML = `<a href="#">
+        <img class="p-8 rounded-t-lg" src="${product.image}"
+          alt="product image" />
+      </a>
+      <div class="px-5 pb-5">
+        <a href="#">
+          <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">${product.name}</h5>
+        </a>
+        <div class="flex items-center mt-2.5 mb-5">
+        ${product.description}
+        </div>
+        <div class="flex items-center justify-between">
+          <span class="text-3xl font-bold text-gray-900 dark:text-white">${product.price}</span>
+          <button
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" value="Add to cart">Add to cart</button>
+        </div>
+      </div>`
+      productList.appendChild(productRaw);
+    });
+    let cartButton = document.querySelectorAll(`button[value="Add to cart"]`);
+    console.log(cartButton);
+    cartButton.forEach((button) => {
+        button.addEventListener('click', addToCart);
+    });
+}
+displayProductList();
+function addToCart() {
+    alert('Button Click');
+}
---
+++ Assignment_4/product.js
@@ -0,0 +1,19 @@
+export const products = [{
+    id: 1,
+    name: 'Digital Camera',
+    price: 50.99,
+    description: 'Editorial, Arts & Culture, Film',
+    image: '/images/camera.png'
+}, {
+    id: 2,
+    name: 'Wireless HeadPhone',
+    price: 49.99,
+    description: 'Editorial, Arts & Culture, Technology',
+    image: '/images/headphone.png'
+}, {
+    id: 3,
+    name: 'Smart Watch',
+    price: 8.99,
+    description: 'Editorial, Arts & Culture, Photography',
+    image: '/images/watch.png'
+} ];
